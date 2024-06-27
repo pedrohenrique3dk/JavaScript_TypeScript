fbe44e4732aaec15dc124f268e31711eb8c45367
Pedro Henrique Martins Belo <pedrohenriqueunicamp2024@gmail.com>
2024-06-27 20:43:39 -0300

    JavaScript & TypeScript
    Aula01 do curso de JavaScript & TypeScript.

---
+++ JavaScript/aula01.js
@@ -0,0 +1,6 @@
+console.log('Pedro Henrique'); //dentro do console.log eu posso colocar qualquer coisa
+console.log("Pedro Henrique"); 
+console.log(`Pedro Henrique`);// utilizado para templates strings (assunto futuro)
+//tudo que é escrito entre aspas ou crase é considerado string (texto)
+
+console.log(123456, 15.85, 'Pedro Henrique'); //Number = Número
